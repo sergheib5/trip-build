ccfc0c3f1559ae0d6fa09413389e662774aac9fd
sergheib5@gmail.com <SRsa3329$@>
2025-04-20 13:42:04 -0500

    local storage added

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
-import { getAuth } from 'firebase/auth'
+import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAKRNuO-mpLBw4s9ZD66_4V1oXuaLWYqMA',
@@ -15,5 +15,11 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 const auth = getAuth(app)
+
+// Add persistence configuration
+setPersistence(auth, browserLocalPersistence)
+  .catch((error) => {
+    console.error('Auth persistence error:', error)
+  })
 
 export { db, auth }
--- src/stores/storeAuth.js
+++ src/stores/storeAuth.js
@@ -10,27 +10,33 @@ import { useRouter } from 'vue-router'
 // import { useStoreNotes } from '@/stores/storeNotes'
 
 export const useStoreAuth = defineStore('storeAuth', {
-  state: () => {
-    return {
-      user: {},
-      router: null,
-    }
-  },
+  state: () => ({
+    user: {},
+    router: null,
+    isInitialized: false,
+  }),
   actions: {
     init() {
       this.router = useRouter()
       // const storeNotes = useStoreNotes()
 
-      onAuthStateChanged(auth, user => {
-        if (user) {
-          this.user.id = user.uid
-          this.user.email = user.email
-          this.router.push('/itinerary')
-        } else {
-          this.user = {}
-          this.router.replace('/auth')
-          // storeNotes.clearNotes()
-        }
+      return new Promise((resolve) => {
+        onAuthStateChanged(auth, (user) => {
+          if (user) {
+            this.user.id = user.uid
+            this.user.email = user.email
+            if (!this.isInitialized) {
+              this.router.push('/itinerary')
+            }
+          } else {
+            this.user = {}
+            if (!this.isInitialized) {
+              this.router.replace('/auth')
+            }
+          }
+          this.isInitialized = true
+          resolve(user)
+        })
       })
     },
     registerUser(credentials) {
